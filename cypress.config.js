b1ada088c7bd7ad02c4d863b2fa06e4ab5499928
Dmytro Zhyvov <dmytro.zhyvov@wikrgroup.com>
2022-11-19 19:56:42 +0200

    added mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,13 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: false,
+    json: true
+  },
   e2e: {
     baseUrl: 'https://automationteststore.com',
     setupNodeEvents(on, config) {
