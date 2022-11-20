cd84f316eddd7d66c26b96b04f476645681f1249
Dmytro Zhyvov <dmytro.zhyvov@wikrgroup.com>
2022-11-20 12:50:13 +0200

    added Allure

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
 
 module.exports = defineConfig({
   viewportHeight: 1080,
@@ -13,7 +15,8 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://automationteststore.com',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
   },
 });
